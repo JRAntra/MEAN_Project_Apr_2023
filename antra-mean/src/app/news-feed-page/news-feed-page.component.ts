9653dce94fac3a834bfa231af19bd9db9a248641
Wenqi Jin <kristenjinn@gmail.com>
2023-05-20 11:55:48 -0700

    created new components directories in src/app for login-page and news-feed-page

---
+++ antra-mean/src/app/news-feed-page/news-feed-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-news-feed-page',
+  templateUrl: './news-feed-page.component.html',
+  styleUrls: ['./news-feed-page.component.sass']
+})
+export class NewsFeedPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
