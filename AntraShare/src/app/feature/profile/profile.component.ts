ed24e6d5ffa7aaecbd7dde2f8ad3696f7661a52f
Tang <TangMutian@users.noreply.github.com>
2023-05-20 16:14:23 +0000

    profile page html and css

--- AntraShare/src/app/feature/profile/profile.component.ts
+++ AntraShare/src/app/feature/profile/profile.component.ts
@@ -3,8 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.sass']
+  styleUrls: ['./profile.component.sass'],
 })
 export class ProfileComponent {
-
+  genderOptions: string[];
+  selectedGender: string = "";
+  constructor() {
+    this.genderOptions = ['Male', 'Female', 'Other'];
+  }
 }
