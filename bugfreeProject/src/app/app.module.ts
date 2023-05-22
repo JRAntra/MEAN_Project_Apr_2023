7fa0b00d5a632f34b905098630385fb6acf73a20
zejun <li.zeju@husky.neu.edu>
2023-05-22 05:23:48 -0400

    admin page first draft, done

--- bugfreeProject/src/app/app.module.ts
+++ bugfreeProject/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SettingPageComponent } from './setting-page-component';
+import { AdminPageComponent } from './admin-page-component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SettingPageComponent,
+    AdminPageComponent,
   ],
   imports: [
     BrowserModule,
