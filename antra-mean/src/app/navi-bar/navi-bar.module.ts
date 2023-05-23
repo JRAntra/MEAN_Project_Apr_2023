668a7b435c84d9e4ae2e8390bbef05949efc55e5
Wenqi Jin <kristenjinn@gmail.com>
2023-05-23 16:24:46 -0700

    practiced creating navi-bar with routing

---
+++ antra-mean/src/app/navi-bar/navi-bar.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NaviBarComponent } from './navi-bar/navi-bar.component';
+
+
+
+@NgModule({
+  declarations: [
+    NaviBarComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class NaviBarModule { }
---
+++ antra-mean/src/app/navi-bar/navi-bar/navi-bar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-navi-bar',
+  templateUrl: './navi-bar.component.html',
+  styleUrls: ['./navi-bar.component.sass']
+})
+export class NaviBarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
