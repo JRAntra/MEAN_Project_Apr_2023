f302423ed5a13da6500bedf46481e97c295fdccf
VirginiaBC <wbc0310@icloud.com>
2023-05-25 11:50:06 -0700

    modify navi and admin style, import necessary antd module for them

--- antra-mean/src/app/feature/admin-page/admin.component.spec.ts
+++ antra-mean/src/app/feature/admin-page/admin.component.spec.ts
@@ -1,22 +1,22 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing'
 
-import { AdminPageComponent } from './admin.component';
+import { AdminPageComponent } from './admin.component'
 
 describe('AdminPageComponent', () => {
-  let component: AdminPageComponent;
-  let fixture: ComponentFixture<AdminPageComponent>;
+	let component: AdminPageComponent
+	let fixture: ComponentFixture<AdminPageComponent>
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [AdminPageComponent],
-    }).compileComponents();
+	beforeEach(async () => {
+		await TestBed.configureTestingModule({
+			declarations: [AdminPageComponent]
+		}).compileComponents()
 
-    fixture = TestBed.createComponent(AdminPageComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+		fixture = TestBed.createComponent(AdminPageComponent)
+		component = fixture.componentInstance
+		fixture.detectChanges()
+	})
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-});
+	it('should create', () => {
+		expect(component).toBeTruthy()
+	})
+})
--- antra-mean/src/app/feature/admin-page/admin.module.ts
+++ antra-mean/src/app/feature/admin-page/admin.module.ts
@@ -2,9 +2,18 @@ import { NgModule } from '@angular/core'
 import { CommonModule } from '@angular/common'
 import { AdminPageComponent } from './admin.component'
 import { NzLayoutModule } from 'ng-zorro-antd/layout'
+import { NzButtonModule } from 'ng-zorro-antd/button'
+import { NzMenuModule } from 'ng-zorro-antd/menu'
+import { NzIconModule } from 'ng-zorro-antd/icon'
 
 @NgModule({
-	imports: [CommonModule, NzLayoutModule],
+	imports: [
+		CommonModule,
+		NzLayoutModule,
+		NzButtonModule,
+		NzMenuModule,
+		NzIconModule
+	],
 	declarations: [AdminPageComponent],
 	exports: [AdminPageComponent]
 })
