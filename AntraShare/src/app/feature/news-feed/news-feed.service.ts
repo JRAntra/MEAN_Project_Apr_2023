ab3efaae7ccc1e98bca051ff5017fc6e1ad93f05
Tang <TangMutian@users.noreply.github.com>
2023-05-26 19:51:20 +0000

    retrieve news from server

--- AntraShare/src/app/feature/news-feed/news-feed.service.ts
+++ AntraShare/src/app/feature/news-feed/news-feed.service.ts
@@ -1,32 +1,39 @@
 import { Injectable, OnInit } from '@angular/core';
 import { Story, Comment } from 'src/app/shared/types';
 import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class NewsFeedService implements OnInit {
-  news_feed: Story[] = [];
+  news_feed_cache: Story[] = [];
   constructor(private http: HttpClient) {}
 
   ngOnInit() {
-    this.news_feed = Array<Story>(3).fill({
-      publisherName: 'test-user',
-      publishedTime: new Date().toString(),
-      content: {
-        text: 'test-content',
-        image: '',
-        video: '',
-      },
-      comments: Array(7).fill({
-        publisherName: 'test-user',
-        publishedTime: new Date().toString(),
-        content: {
-          text: 'test-content',
-          image: '',
-          video: '',
-        },
-      }),
+    //TODO
+  }
+
+  _fetchNewsFeed() {
+    // TODO: error handling
+    return new Observable<Story[]>((subscriber) => {
+      this.http
+        .get<Story[]>(`${environment.apiUrl}/api/news`)
+        .subscribe((data: Story[]) => {
+          this.news_feed_cache = data;
+          subscriber.next(data);
+        });
     });
+  }
+
+  getNewsFeed() {
+    if (this.news_feed_cache.length === 0) {
+      return this._fetchNewsFeed();
+    } else {
+      return new Observable<Story[]>((subscriber) => {
+        subscriber.next(this.news_feed_cache);
+      });
+    }
   }
 }
