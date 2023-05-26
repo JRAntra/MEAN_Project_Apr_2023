9e08fbd15852f7d49c4e20f785c61a1e5f2dfb4c
VirginiaBC <wbc0310@icloud.com>
2023-05-26 11:45:29 -0700

    Create Routing system for all different modules with path. Enable Navigation Bar which can navigate to different pages.

--- antra-mean/src/app/feature/setting-page/setting.module.ts
+++ antra-mean/src/app/feature/setting-page/setting.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common'
 import { SettingPageComponent } from './setting-page.component'
 import { NzButtonModule } from 'ng-zorro-antd/button'
 import { NzIconModule } from 'ng-zorro-antd/icon'
+import { RouterModule } from '@angular/router'
 
 @NgModule({
-	imports: [CommonModule, NzButtonModule, NzIconModule],
+	imports: [CommonModule, NzButtonModule, NzIconModule, RouterModule],
 	declarations: [SettingPageComponent],
 	exports: [SettingPageComponent]
 })
