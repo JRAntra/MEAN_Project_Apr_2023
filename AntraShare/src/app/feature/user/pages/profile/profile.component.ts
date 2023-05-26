3af8e62d3b2d9f1946bfde7f88a17234d38bfe94
Tang <TangMutian@users.noreply.github.com>
2023-05-26 23:08:50 +0000

    add interaction in profile page

--- AntraShare/src/app/feature/user/pages/profile/profile.component.ts
+++ AntraShare/src/app/feature/user/pages/profile/profile.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Inplace } from 'primeng/inplace';
 
 @Component({
   selector: 'app-profile',
@@ -6,9 +7,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./profile.component.sass'],
 })
 export class ProfileComponent {
+  loading = false;
   genderOptions: string[];
   selectedGender = '';
   constructor() {
     this.genderOptions = ['Male', 'Female', 'Other'];
+  }
+
+  save(event: MouseEvent, ref: Inplace) {
+    this.loading = true;
+    setTimeout(() => {
+      ref.deactivate(event);
+      this.loading = false;
+    }, 1000);
   }
 }
