d39d48148b09bd7061366bad38196689da273766
Allister <allisterakun@gmail.com>
2023-05-26 23:26:39 -0400

    admin works

---
+++ AntraShare/src/app/feature/admin/admin.service.ts
@@ -0,0 +1,21 @@
+import { UserProfile } from 'src/app/shared/types';
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+import { map } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AdminService {
+  constructor(private http: HttpClient) { }
+
+  getAllUser() {
+    return this.http.get(`${environment.apiUrl}/api/users/getAllUsers`, { observe: 'response' })
+      .pipe(
+        map((response) => {
+          return (<UserProfile[]>response.body);
+        }),
+      );
+  }
+}
