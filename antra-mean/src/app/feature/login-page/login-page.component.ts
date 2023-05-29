a3384ae5b066f910cc767eb2d18204c37d6affcd
Wenqi Jin <kristenjinn@gmail.com>
2023-05-29 16:48:07 -0700

    login service is done, implemented notifications

--- antra-mean/src/app/feature/login-page/login-page.component.ts
+++ antra-mean/src/app/feature/login-page/login-page.component.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core'
 import { FormGroup, FormControl } from '@angular/forms'
+import { LoginService } from './login.service'
+import { Router } from '@angular/router'
+import { NzNotificationService } from 'ng-zorro-antd/notification'
+
 @Component({
 	selector: 'app-login-page',
 	templateUrl: './login-page.component.html',
@@ -8,11 +12,59 @@ import { FormGroup, FormControl } from '@angular/forms'
 export class LoginPageComponent {
 	myFormGroup: FormGroup
 
-	constructor() {
+	constructor(private notification: NzNotificationService, private loginService: LoginService, private router: Router) {
 		this.myFormGroup = new FormGroup({
 			userName: new FormControl(),
 			password: new FormControl(),
 			agreement: new FormControl()
 		})
 	}
+	agreeNotification(): void {
+    this.notification.create(
+      'warning',
+      'Warning',
+      'Please agree to the terms and conditions.',
+    )
+  }
+	wrongLoginNotification(): void {
+    this.notification.create(
+      'error',
+      'Error',
+      'Invalid username or password.',
+    )
+  }
+	successLoginNotification(): void {
+    this.notification.create(
+      'success',
+      'Success',
+      'You have logged in.',
+    )
+  }
+	login() {
+    const username = this.myFormGroup.get('userName')?.value
+    const password = this.myFormGroup.get('password')?.value
+		const agree = this.myFormGroup.get('agreement')?.value
+		if (!agree) {
+      this.agreeNotification()
+			return
+    }
+		if (username && password && agree) {
+			this.loginService.loginUser(username, password).subscribe(
+				response => {
+					if (response) {
+						// Handle the successful login response
+						this.successLoginNotification()
+						this.router.navigate(['/news-feed'])
+						console.log(response)
+					} else {
+						this.wrongLoginNotification()
+         }
+				},
+				error => {
+					// Handle the login error
+					console.error(error)
+				}
+			)
+		}
+  }
 }
--- antra-mean/src/app/feature/news-feed/news-feed.service.ts
+++ antra-mean/src/app/feature/news-feed/news-feed.service.ts
@@ -1,18 +1,17 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Injectable } from '@angular/core'
+import { HttpClient } from '@angular/common/http'
+import { Observable } from 'rxjs'
 
 
 @Injectable({
   providedIn: 'root'
 })
 export class NewsFeedService {
-  private apiUrl = 'https://dummyjson.com/posts';
+  private apiUrl = 'https://dummyjson.com/posts'
 
   constructor(private http: HttpClient) { }
 
   getNewsFeed(): Observable<any[]> {
-    return this.http.get<any>(this.apiUrl);
+    return this.http.get<any>(this.apiUrl)
   }
 }
-
