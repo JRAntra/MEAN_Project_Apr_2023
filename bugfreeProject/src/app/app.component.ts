8848bb898f8f71fb5cb38f01bd08290cf941e5cc
maria3213 <fc625@georgetown.edu>
2023-05-29 16:45:28 -0400

    create modules and setup routings

--- bugfreeProject/src/app/app.component.ts
+++ bugfreeProject/src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass']
 })
+
 export class AppComponent {
   title = 'bugfreeProject';
+  constructor(private router: Router) { }
+
+  login() {
+    this.router.navigate(['login']);
+  }
+
+  submit(testForm: any){
+    console.log(testForm.value);
+  }
 }
