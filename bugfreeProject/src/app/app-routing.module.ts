90b9c6e5005687746913c5c85a4b89ae3d9d2253
zejun <li.zeju@husky.neu.edu>
2023-05-29 03:23:49 -0400

    setting page can click button to route to admin page now

--- bugfreeProject/src/app/app-routing.module.ts
+++ bugfreeProject/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SettingPageComponent } from './setting/setting-page-component';
+import { AdminPageComponent } from './admin/components/admin/admin-page-component';
 
-const routes: Routes = [];
+const routes: Routes = [
+    {path: 'setting', component: SettingPageComponent},
+    {path: 'admin', component: AdminPageComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- bugfreeProject/src/app/setting/setting.module.ts
+++ bugfreeProject/src/app/setting/setting.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SettingPageComponent } from './setting-page-component';
+import { RouterModule } from '@angular/router';
 
 
 @NgModule({
@@ -8,7 +9,8 @@ import { SettingPageComponent } from './setting-page-component';
         SettingPageComponent,
     ],
     imports: [
-        CommonModule
+        CommonModule,
+        RouterModule,
     ],
     exports: [
         SettingPageComponent,
