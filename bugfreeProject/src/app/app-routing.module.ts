b339639e5b506902a3bfae22286e5e3f9baa0092
maria3213 <fc625@georgetown.edu>
2023-05-30 08:24:19 -0400

    update components

--- bugfreeProject/src/app/app-routing.module.ts
+++ bugfreeProject/src/app/app-routing.module.ts
@@ -1,13 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { ErrorComponent } from './error/error/error.component';
 import { LoginComponent } from './login/login/login.component';
 import { RegisterComponent } from './register/register/register.component';
 
 const routes: Routes = [
   {path: 'login', component: LoginComponent},
-  {path: 'register', component: RegisterComponent},
-  {path:'error', component: ErrorComponent}
+  {path: 'register', component: RegisterComponent}
 ];
 
 @NgModule({
--- bugfreeProject/src/app/app.module.ts
+++ bugfreeProject/src/app/app.module.ts
@@ -1,20 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
-import { RegisterComponent } from './register/register.component';
+// import { ProductsModule } from './products/products.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { RegisterModule } from './register/register.module';
+import { LoginModule } from './login/login/login.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent,
-    RegisterComponent
+
   ],
   imports: [
+    RegisterModule,
+    LoginModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
