8542b852694bd3c32ffad3f494f299f760bd09b4
maria3213 <fc625@georgetown.edu>
2023-05-30 13:01:34 -0400

    update login

--- bugfreeProject/src/app/login/login/login.component.ts
+++ bugfreeProject/src/app/login/login/login.component.ts
@@ -1,6 +1,5 @@
 import { Component} from '@angular/core';
 import { LoginCheckService } from '../login-check.service';
-import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -8,34 +7,34 @@ import { Router } from '@angular/router';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent{
-  login_info: any = {
+  inputData: any = {
     userEmail: '',
     password: '',
     agree: false,
   };
 
-  constructor(private loginCheckService : LoginCheckService,
-              private router:Router) { }
+  constructor(private loginCheckService : LoginCheckService) { }
 
   submit(testForm: any){
     console.log(testForm.value);
-    this.login_info = testForm.value;
-    if (this.validateEmail() && this.login_info.agree){
-      this.loginCheckService.login(this.login_info).subscribe({
+    this.inputData = testForm.value;
+    if (this.validateEmail() && this.inputData.agree){
+      this.loginCheckService.login(this.inputData).subscribe({
           next:(res) => { if (res.status === 200) {
-              console.log("200!!");
-              this.router.navigate(['register']);
+              alert("Login Success...");
+              testForm.reset();
+              // this.router.navigate(['news-feed']);
             }},
           error:(error) => {alert(error.error)},
       });
-    }else if(!this.login_info.agree){
+    }else if(!this.inputData.agree){
       alert("Please agree to Agreement!");
-      // testForm.reset();
+      
     }
  
   }
   validateEmail() {
-    if (this.login_info.agree && /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(this.login_info.userEmail!)) {
+    if (this.inputData.agree && /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(this.inputData.userEmail!)) {
       return true;
     } else{
       return false;
