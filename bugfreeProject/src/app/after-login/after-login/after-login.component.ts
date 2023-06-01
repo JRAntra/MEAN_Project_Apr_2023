6c5e310bd9179f0e0bf11e87d9a9ff6ab2c94ca2
upuphero <540864988@qq.com>
2023-05-31 22:22:34 -0400

    create the service to retrieve the news

--- bugfreeProject/src/app/after-login/after-login/after-login.component.ts
+++ bugfreeProject/src/app/after-login/after-login/after-login.component.ts
@@ -1,14 +1,28 @@
-import { Component } from '@angular/core';
+import { Component , OnInit} from '@angular/core';
+import { NewsFeedService,NewsItem } from '../../core/services/news-feed.service';
 
 @Component({
   selector: 'app-after-login',
   templateUrl: './after-login.component.html',
   styleUrls: ['./after-login.component.scss']
 })
-export class AfterLoginComponent {
+
+export class AfterLoginComponent implements OnInit {
   searchText = '';
+  //newsItem: NewsItem | undefined;
+  //newsFeedData: any[] = [];
+  newsFeed: NewsItem[] = [];
+  constructor(private newsFeedService: NewsFeedService) {}
+  
+  ngOnInit(): void {
+    this.newsFeedService.getNewsFeed().subscribe(data => {
+      this.newsFeed = data;
+      console.log(this.newsFeed);
+    });
+  }
   onSearch(){
     console.log('Searching for:', this.searchText);
   }
+
 
 }
