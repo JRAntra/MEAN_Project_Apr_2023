4c2855cdef8e53c33b0e31f834e8ed6c54817ad1
Wenqi Jin <kristenjinn@gmail.com>
2023-06-06 16:09:03 -0700

    admin auth guard completed

---
+++ AntraShare/src/app/shared/auth.guard.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+import { Router } from '@angular/router';
+import { AdminService } from '../feature/admin/admin.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class AuthGuard implements CanActivate {
+
+  constructor(private adminService: AdminService, private _router: Router) {
+
+  }
+
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): boolean | UrlTree | Observable<boolean | UrlTree> | Promise<boolean | UrlTree> {
+      console.log("in Auth guard::::")
+      console.log(this.adminService.adminAuthorized())
+      let val = this.adminService.adminAuthorized()
+
+      if (val) {
+        return true
+      } else {
+        alert("Please leave this page. You are not authorized to be here.")
+        return false
+      }
+  }
+};
