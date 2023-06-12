b46432bd12be6299ca7662004da320a134403fb5
VirginiaBC <wbc0310@icloud.com>
2023-06-11 20:35:24 -0700

    add routing

--- antra-mean/src/app/feature/admin-page/admin.service.ts
+++ antra-mean/src/app/feature/admin-page/admin.service.ts
@@ -2,14 +2,28 @@ import { UserInfo } from 'src/app/shared/userInfo.model'
 import { Injectable } from '@angular/core'
 import { HttpClient } from '@angular/common/http'
 import { map } from 'rxjs/operators'
+import { UserAccountService } from 'src/app/shared/userAccountService'
 
 @Injectable({
 	providedIn: 'root'
 })
 export class AdminService {
 	private apiUrl = 'http://localhost:4231/api/users/getAllUsers'
+	isAdmin = false
 
-	constructor(private http: HttpClient) {}
+	constructor(
+		private http: HttpClient,
+		private userAccount: UserAccountService
+	) {}
+
+	changeRole() {
+		const user = this.userAccount.getLocalUserInfo()
+		console.log(user)
+	}
+
+	isAuthorized() {
+		return this.isAdmin
+	}
 
 	getAllUser() {
 		return this.http.get<any>(this.apiUrl).pipe(
